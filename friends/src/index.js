0c711cd95db1aeeda85c2d1ec38a95b86cb1e952
sam-malanchuk <malanchuksam@gmail.com>
2019-07-18 20:08:51 -0400

    initialized the project. imported redux react-redux redux-thunk redux-logger and set up in index.js

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reducer from './reducer'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { logger } from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducer,
+    // using compose to use more than one middlewhere object
+    compose(
+		// redux middleware
+		applyMiddleware(thunk, logger),
+		// redux dev tools middleware
+		window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+	),
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
---
+++ friends/src/reducer.js
@@ -0,0 +1,16 @@
+// import action types
+// import {
+// } from './actions'
+
+// state initial values
+const initialState = {
+    friends: [],
+}
+
+export default function(state = initialState, action) {
+	switch (action.type) {
+        //  insert reducer cases here
+        default:
+            return state
+    }
+}
