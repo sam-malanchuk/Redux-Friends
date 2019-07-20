921f891bb081b53539d654cb847134bd2e7c922c
sam-malanchuk <malanchuksam@gmail.com>
2019-07-20 12:00:01 -0400

    added logout button on dashboard only when logged in

--- friends/src/components/Dashboard.js
+++ friends/src/components/Dashboard.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 
-function Dashboard() {
-  return (
-    <div>
-        <h3>Dashboard page</h3>
-    </div>
-  );
+class Dashboard extends React.Component {
+    token = localStorage.getItem('token')
+
+    logout = (evt) => {
+		evt.preventDefault()
+
+		localStorage.removeItem('token')
+		this.props.history.push('/login')
+	}
+
+    render() {
+		return (
+        <div>
+            <h3>Dashboard page</h3>
+            { // show logout button only when logged in
+                this.token && <button type="button" onClick={this.logout}>Logout</button>}
+        </div>
+        );
+    }
 }
 
-export default Dashboard;
+export default withRouter(Dashboard);
